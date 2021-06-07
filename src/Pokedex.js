93d3f08f7ecfa2955c8ba59621f9c80d631a3ad1
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2021-06-07 06:30:32 -0500

    refactored some code

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
-import uuid from "uuid/dist/v4";
+
 import Pokecard from "./Pokecard";
-import Pokeinfo from "./Pokeinfo";
-//import Pokename from "./Pokename";
+
+import RenderPokeInfo from "./RenderPokeInfo";
+
 import TypeInfo from "./types.json";
-import PokeJSON from "./pokemon.json";
 
 import Pokenav from "./Pokenav";
 import "./Pokedex.css";
@@ -92,9 +92,6 @@ class Pokedex extends Component {
     } else {
       document.querySelector(".Pokedex-dropdown-items ").style.display = "none";
     }
-
-    //x.classList.toggle("menu-active");
-    //alert("you clicked the menu toggle btn");
   }
 
   closeFormSel() {
@@ -219,143 +216,11 @@ class Pokedex extends Component {
             <div className="Pokedex-pokemon">{generatePokemon}</div>
           </div>
         ) : (
-          <div>
-            <div className="Pokedex-info-container">
-              <div className="Pokedex-header">
-                <div className="Pokedex-cntrl">
-                  <div
-                    className="Pokedex-prev"
-                    onClick={() =>
-                      PokeJSON[
-                        Number(
-                          currentPokemon.varData[0].url
-                            .replace("https://pokeapi.co/api/v2/pokemon/", "")
-                            .replace("/", "")
-                        ) - 2
-                      ] !== undefined &&
-                      this.handleInfo(
-                        `${API_URl}${Number(
-                          currentPokemon.species.url.split("/")[6] - 1
-                        )}/`,
-                        Number(currentPokemon.species.url.split("/")[6] - 1)
-                      )
-                    }
-                  >
-                    <i className="fas fa-angle-left"></i>
-                  </div>
-                  <span>
-                    {PokeJSON[
-                      Number(
-                        currentPokemon.varData[0].url
-                          .replace("https://pokeapi.co/api/v2/pokemon/", "")
-                          .replace("/", "")
-                      ) - 2
-                    ] === undefined
-                      ? ""
-                      : PokeJSON[
-                          Number(
-                            currentPokemon.varData[0].url
-                              .replace("https://pokeapi.co/api/v2/pokemon/", "")
-                              .replace("/", "")
-                          ) - 2
-                        ]}
-                  </span>
-                </div>
-
-                <div className="Pokedex-dropdown">
-                  <div className="Pokedex-dropdown-menu">
-                    <span>
-                      {currentPokemon.name[0][0].toUpperCase() +
-                        currentPokemon.name.substring(1)}
-                    </span>
-                    <i
-                      className=" menu-toggle fas fa-angle-down"
-                      onClick={this.handleMenuBtn}
-                    ></i>
-                    {/* forms={currentPokemon.varData} */}
-                  </div>
-                  <div className="Pokedex-dropdown-items">
-                    <ul>
-                      {currentPokemon.varData.map((form) => (
-                        <li
-                          key={form.name}
-                          onClick={() =>
-                            this.handleInfo(form.url, form.name.split("-")[0])
-                          }
-                        >
-                          {form.name[0].toUpperCase() + form.name.substring(1)}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-                <div className="Pokedex-cntrl">
-                  <span>
-                    {PokeJSON[
-                      Number(
-                        currentPokemon.varData[0].url
-                          .replace("https://pokeapi.co/api/v2/pokemon/", "")
-                          .replace("/", "")
-                      )
-                    ] === undefined
-                      ? ""
-                      : PokeJSON[
-                          Number(
-                            currentPokemon.varData[0].url
-                              .replace("https://pokeapi.co/api/v2/pokemon/", "")
-                              .replace("/", "")
-                          )
-                        ]}
-                  </span>
-                  <div
-                    className="Pokedex-next"
-                    onClick={() =>
-                      PokeJSON[
-                        Number(
-                          currentPokemon.varData[0].url
-                            .replace("https://pokeapi.co/api/v2/pokemon/", "")
-                            .replace("/", "")
-                        )
-                      ] !== undefined &&
-                      this.handleInfo(
-                        `${API_URl}${
-                          Number(currentPokemon.species.url.split("/")[6]) + 1
-                        }/`,
-                        Number(currentPokemon.species.url.split("/")[6]) + 1
-                      )
-                    }
-                  >
-                    <i className="fas fa-angle-right"></i>
-                  </div>
-                </div>
-              </div>
-              {/*    <Pokename
-                name={currentPokemon.name}
-                varieties={currentPokemon.varData}
-                handleInfo={this.handleInfo}
-                id={currentPokemon.id}
-                key={uuid()}
-                img={currentPokemon.sprites.front_default}
-              />
- */}
-              <Pokeinfo
-                img={currentPokemon.sprites.front_default}
-                types={currentPokemon.types}
-                name={currentPokemon.name}
-                weight={currentPokemon.weight}
-                height={currentPokemon.height}
-                abilities={currentPokemon.abilities}
-                id={currentPokemon.id}
-                key={uuid()}
-                stats={currentPokemon.stats}
-                forms={currentPokemon.varData}
-                species={currentPokemon.species}
-                desc={currentPokemon.desc}
-                evolution={currentPokemon.evolution_chain}
-                handleInfo={this.handleInfo}
-              />
-            </div>
-          </div>
+          <RenderPokeInfo
+            handleMenuBtn={this.handleMenuBtn}
+            handleInfo={this.handleInfo}
+            currentPokemon={currentPokemon}
+          />
         )}
         <div className="Pokedex-footer">
           <p>
